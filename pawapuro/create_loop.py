4b8201e9de3b2ee06c379286d3185bcd2fcabc41
Shigi-p <shigi.522@gmail.com>
2020-10-19 22:36:45 +0900

    [update] blockedとinterleavedに対応

--- pawapuro/create_loop.py
+++ pawapuro/create_loop.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 
+import random
+
 if __name__ == "__main__":
     with open('./loop.txt', mode='w') as f:
         f.write('1\n')  # ゲームスタート
@@ -13,6 +15,20 @@ if __name__ == "__main__":
         f.write('\n'*11) # 会話
         # for _ in range(31):
         #     f.write('1\n' + '\n'*3)  # 練習と結果
-        for i in range(31):
-            f.write(str((i % 4) + 1) + '\n' + '\n' * 10)
         
+        randInteger = random.randint(1, 4)
+        
+        for i in range(31):
+            # f.write(str((i % 4) + 1) + '\n' + '\n' * 10)
+            # f.write(str(random.randint(1, 4)) + '\n' + '\n' * 10)
+            f.write(str(((randInteger + i) % 4) + 1) + '\n' + '\n' * 10)
+       
+        """ for i in range(8):
+            f.write(str((randInteger + 1 )% 4 + 1) + '\n' + '\n' * 10)
+        for i in range(8):
+            f.write(str((randInteger + 2 ) % 4 + 1) + '\n' + '\n' * 10)
+        for i in range(8):
+            f.write(str((randInteger + 3 ) % 4 + 1) + '\n' + '\n' * 10)
+        for i in range(8):
+            f.write(str((randInteger + 4 ) % 4 + 1) + '\n' + '\n' * 10)
+        """
